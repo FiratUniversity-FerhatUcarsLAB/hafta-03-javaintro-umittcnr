44867080136ffc1a1897f7c131a476408d62d238
ümitcan <cinarx04@gmail.com>
2025-10-17 16:42:36 +0300

    Hosgeldinmesajlari.java

---
+++ Task-1-HosgeldinMesajlari/BesKereTekrar.java
@@ -0,0 +1,18 @@
+250541009
+ümitcan çinar
+public class JavayaHosGeldiniz {
+
+		public static void main(String[] args) {
+			System.out.println("Java'ya Hoş Geldiniz");
+			System.out.println("Java'ya Hoş Geldiniz");
+			System.out.println("Java'ya Hoş Geldiniz");
+			System.out.println("Java'ya Hoş Geldiniz");
+			System.out.println("Java'ya Hoş Geldiniz");
+			
+			System.out.println("Bilgisayar Bilimine Hos Geldiniz");
+		
+			System.out.println("Programlama Eglencelidir");
+		
+		
+		}
+}
