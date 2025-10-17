7e313cc4c1d7494ace0108fca7a858f64b6db8cf
ümitcan <cinarx04@gmail.com>
2025-10-17 17:01:08 +0300

    UcMesaj.java

---
+++ Task-1-HosgeldinMesajlari/UcMesaj.java
@@ -0,0 +1,10 @@
+250541009
+ümitcan çinar
+    public class JavayaHosGeldiniz {
+
+		public static void main(String[] args) {
+			System.out.println("Java’ya Hos Geldiniz");
+			System.out.println("Bilgisayar Bilimine Hos Geldiniz");
+			System.out.println("Programlama Eglencelidir");		
+		}
+}
