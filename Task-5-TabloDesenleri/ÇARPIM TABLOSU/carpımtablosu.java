8e451bf895b883037d9dfcb15018d5cc558e6515
ümitcan <cinarx04@gmail.com>
2025-10-20 19:01:24 +0300

    carpımtablosu.java

---
+++ Task-5-TabloDesenleri/ÇARPIM TABLOSU/carpımtablosu.java
@@ -0,0 +1,20 @@
+package carpımtablosu;
+
+public class Carpımtablosu {
+	public static void main(String[] args) {
+		int x = 2;
+		System.out.println("ÇARPIM TABLOSU");
+		System.out.println("==============");
+		System.out.println("2 NİN  KATLARI");
+		System.out.println("1için "+x*1);
+		System.out.println("2için "+x*2);
+		System.out.println("3için "+x*3);
+		System.out.println("4için "+x*4);
+		System.out.println("5için "+x*5);
+		System.out.println("6için "+x*6);
+		System.out.println("7için "+x*7);
+		System.out.println("8için "+x*8);
+		System.out.println("9için "+x*9);
+		System.out.println("10için "+x*10);
+	}
+}
