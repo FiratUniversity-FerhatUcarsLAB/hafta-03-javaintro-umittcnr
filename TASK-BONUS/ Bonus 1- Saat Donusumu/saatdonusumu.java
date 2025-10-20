180768ff1705f0faaed11d9717feae8a2e1dcfc7
ümitcan <cinarx04@gmail.com>
2025-10-20 20:08:28 +0300

    saatdonusumu.java

---
+++ TASK-BONUS/ Bonus 1- Saat Donusumu/saatdonusumu.java
@@ -0,0 +1,14 @@
+package saatdonusturme;
+
+public class saatdonusturme {
+	public static void main(String[] args) {
+	       
+	        int toplamSaniye = 3665;
+
+	        int TOPLAMSAAT = toplamSaniye / 3600;            
+	        int TOPLAMDAKIKA = toplamSaniye/ 60; 
+	        int TOPLAMSANİYE = toplamSaniye/1  ;         
+System.out.println(TOPLAMSANİYE +("SANİYE")+("=") + TOPLAMSAAT +("SAAT EDER ") +TOPLAMDAKIKA +("DAKİKA EDER ")+TOPLAMSANİYE +("SANİYE EDER"));
+	}
+
+}
