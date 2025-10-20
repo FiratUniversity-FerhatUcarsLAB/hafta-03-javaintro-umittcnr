12df50be8972ca0ee139a2deedeb66a171d6e900
ümitcan <cinarx04@gmail.com>
2025-10-20 15:58:04 +0300

    dikdortgen.java

--- Task-3-GeometrikHesaplamalar/ Dikdortgen/dikdortgen.java
+++ Task-3-GeometrikHesaplamalar/ Dikdortgen/dikdortgen.java
@@ -5,6 +5,8 @@ public class dikdortgen {
        double en=4.5 ;
        double boy=7.9 ;
        double alan=en*boy ;
+       double çevre= 2*en +2*boy;
        System.out.println("dikdörtgenin alanı= " +alan);
+	   System.out.println("dikdörtgenin çevresi= " +çevre );
 	}
 }
