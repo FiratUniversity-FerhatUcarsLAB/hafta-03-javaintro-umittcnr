e454e96fa566e78586d21ef7ecb9f6607721e38f
ümitcan <cinarx04@gmail.com>
2025-10-20 09:50:20 +0300

    5tekrarmesaj.java

---
+++ Task-1-HosgeldinMesajlari/5TekrarMesaj/5tekrarmesaj.java
@@ -0,0 +1,10 @@
+public class JavayaHosGeldiniz {
+
+		public static void main(String[] args) {
+			System.out.println("Java'ya Hoş Geldiniz");
+			System.out.println("Java'ya Hoş Geldiniz");
+			System.out.println("Java'ya Hoş Geldiniz");
+			System.out.println("Java'ya Hoş Geldiniz");
+			System.out.println("Java'ya Hoş Geldiniz");
+		}
+}
