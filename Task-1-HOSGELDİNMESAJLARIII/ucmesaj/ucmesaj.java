4091a5b3eaddc6a000eae3f95c4f4935c879487c
ümitcan <cinarx04@gmail.com>
2025-10-20 10:45:48 +0300

    ucmesaj.java

--- Task-1-HOSGELDİNMESAJLARIII/ucmesaj/ucmesaj.java
+++ Task-1-HOSGELDİNMESAJLARIII/ucmesaj/ucmesaj.java
@@ -1,1 +1,10 @@
+package ucmesaj;
+
+public class ucmesaj {
+	public static void main(String[] args) {
+		System.out.println("Java’ya Hos Geldiniz");
+		System.out.println("Bilgisayar Bilimine Hos Geldiniz");
+		System.out.println("Programlama Eglencelidir");		
+	}
+}
 
