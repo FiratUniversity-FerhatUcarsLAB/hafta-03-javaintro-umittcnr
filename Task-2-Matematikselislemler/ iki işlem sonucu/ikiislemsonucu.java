502353d412c430abce45fbcca1b9e292a86e52b2
ümitcan <cinarx04@gmail.com>
2025-10-20 11:23:28 +0300

    ikiislemsonucu.java

---
+++ Task-2-Matematikselislemler/ iki işlem sonucu/ikiislemsonucu.java
@@ -0,0 +1,11 @@
+package ikiislemsonucu;
+
+public class İkiislemsonucu {
+	 public static void main(String[] args) {
+		 double pay = (9.5*4.5)- (2.5*3);
+		 double payda = 45.5- 3.5;
+		 double sonuc = pay / payda;
+		 System.out.println("işlemin sonucu= "+sonuc );
+
+	 }
+}
