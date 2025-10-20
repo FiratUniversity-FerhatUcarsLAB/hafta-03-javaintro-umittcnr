a14749178c422c3d61c62e517aca3adff79d4362
ümitcan <cinarx04@gmail.com>
2025-10-20 10:24:05 +0300

    Add welcome messages in ucmesaj.java

--- Task-1-HOSGELDİNMESAJLARIII/UCMESAJ/ucmesaj.java
+++ Task-1-HOSGELDİNMESAJLARIII/UCMESAJ/ucmesaj.java
@@ -1,1 +1,8 @@
+    public class JavayaHosGeldiniz {
 
+		public static void main(String[] args) {
+			System.out.println("Java’ya Hos Geldiniz");
+			System.out.println("Bilgisayar Bilimine Hos Geldiniz");
+			System.out.println("Programlama Eglencelidir");		
+		}
+}
