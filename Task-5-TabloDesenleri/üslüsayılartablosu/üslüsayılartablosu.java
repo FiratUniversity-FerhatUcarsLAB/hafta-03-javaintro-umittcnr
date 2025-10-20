a2a41378f2c9ea9ae700e8c091cd2841ee9e24d8
ümitcan <cinarx04@gmail.com>
2025-10-20 17:26:48 +0300

    üslüsayılartablosu.java

---
+++ Task-5-TabloDesenleri/üslüsayılartablosu/üslüsayılartablosu.java
@@ -0,0 +1,29 @@
+package karmasıkcıktıformatları;
+
+public class Karmasıkcıktıformatları {
+	public static void main(String[] args) {
+				int a1=1;
+				int a2=1*1;
+				int a3=1*1*1;
+				int b1=2;
+				int b2=2*2;
+				int b3=2*2*2;
+				int c1=3;
+				int c2=3*3;
+				int c3=3*3*3;
+				int d1=4;
+				int d2=4*4;
+				int d3=4*4*4;
+				int e1=5;
+				int e2=5*5;
+				int e3=5*5*5;
+				System.out.println("üslü sayılar tablosu");
+				System.out.println("====================");
+				System.out.println("x"+"      "+"x^1" +"    "+"x^2"+"    "  +"x^3" );
+				System.out.println("x=1"+"   "+a1+"       "+a2+"       "+a3 );
+				System.out.println("x=2"+"   "+b1+"       "+b2+"       "+b3  );
+				System.out.println("x=3"+"   "+c1+"       "+c2+"       "+c3 );
+				System.out.println("x=4"+"   "+d1+"       "+d2+"       "+d3 );
+				System.out.println("x=5"+"   "+e1+"       "+e2+"       "+e3 );
+	}
+}
