fe74ba738d4fadcfbe0a8851bd61cfa52327bdb0
ümitcan <cinarx04@gmail.com>
2025-10-23 10:15:43 +0300

    Update DaireHesap.java

---
+++ Task-3-GeometrikHesaplamalar/DaireHesap.java
@@ -0,0 +1,14 @@
+package daire;
+
+public class daire {
+	 public static void main(String[] args) {
+		 double r = 5.5;
+		 double pi = 3.14159;
+		 double alan = pi * r * r;
+		 double cevre = 2 * pi * r;
+		 System.out.println("Yaricap: " + r);
+		 System.out.println("Alan: " + alan);
+		 System.out.println("Cevre: " + cevre);
+	 }
+
+}
