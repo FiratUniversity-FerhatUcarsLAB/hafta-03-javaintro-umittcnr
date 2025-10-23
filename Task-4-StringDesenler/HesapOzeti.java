701b114ab901a94f41833856878708e8a24cc970
ümitcan <cinarx04@gmail.com>
2025-10-23 10:20:42 +0300

    Update HesapOzeti.java

---
+++ Task-4-StringDesenler/HesapOzeti.java
@@ -0,0 +1,24 @@
+
+package hesapozeti;
+
+public class Hesapozeti {
+	 public static void main(String[] args) {
+		String ÜRÜN1= "elma/" ;
+		String ÜRÜN2= "armut/";
+		String ÜRÜN3= "portakal/";
+		String X= "10/";
+		String Y="20/";
+		String Z ="30/";
+		String A ="1/";
+		String B ="2/";
+		String C ="3/";
+		 System.out.println("ALIŞVERİŞ LİSTESİ");
+		 System.out.println("===================");
+		 System.out.println("ÜRÜNLER: " +ÜRÜN1 + ÜRÜN2 + ÜRÜN3);
+		 System.out.println("===================");
+		 System.out.println("FİYATLAR(tl): " +X    +  Y   +   Z);
+	     System.out.println("===================");
+		 System.out.println("MİKTARLAR(KG): "+A      +B    +  C);
+		 System.out.println("===================");	 
+	 }
+}
