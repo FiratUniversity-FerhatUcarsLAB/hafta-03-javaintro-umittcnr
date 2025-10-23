6ac465b83bddec94bcab6d5b88ab792f36527472
ümitcan <cinarx04@gmail.com>
2025-10-23 10:19:40 +0300

    Update AsciiArtHarf.java

---
+++ Task-4-StringDesenler/AsciiArtHarf.java
@@ -0,0 +1,15 @@
+public class Ascııart {
+    public static void main(String[] args) {
+        System.out.println("/"+"           "+"/");
+        System.out.println("/"+"           "+"/");
+        System.out.println("/"+"           "+"/");
+        System.out.println("/"+"           "+"/");
+        System.out.println("/"+"           "+"/");
+        System.out.println("/"+"           "+"/");
+        System.out.println("/"+"           "+"/");
+        System.out.println(" /"+"         "+"/ ");
+        System.out.println("  /"+"       "+"/  ");
+        System.out.println("    /"+"   "+"/    ");
+        System.out.println("      /         ");
+    }
+}
