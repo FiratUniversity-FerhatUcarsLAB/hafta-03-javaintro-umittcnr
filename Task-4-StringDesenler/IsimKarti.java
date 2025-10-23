ceee48cba1ae6daa78cdcee4f45f8b49e4af9f0b
ümitcan <cinarx04@gmail.com>
2025-10-23 10:20:07 +0300

    Update IsimKarti.java

---
+++ Task-4-StringDesenler/IsimKarti.java
@@ -0,0 +1,20 @@
+package isimkartı;
+
+public class isimkartı {
+	 public class IsimKarti {
+		 public static void main(String[] args) {
+		 String ad = "ümitcan";
+		 String soyad = "çinar";
+		 String ogrNo = "250541009";
+		 String bolum = "yazılım müh.";
+		 System.out.println("===================");
+		 System.out.println(" OGRENCI BILGILERI ");
+		 System.out.println("===================");
+		 System.out.println("Ad : " +ad);
+		 System.out.println("Soyad : " +soyad);
+		 System.out.println("No: " + ogrNo);
+		 System.out.println("Bolum : " + bolum);
+		 System.out.println("===================");
+		 }
+	 }	 
+}
