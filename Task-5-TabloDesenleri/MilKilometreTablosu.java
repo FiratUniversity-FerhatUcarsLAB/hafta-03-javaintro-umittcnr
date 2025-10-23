9d3b3cbd003198c15fea25929c6f1d1b87e31188
ümitcan <cinarx04@gmail.com>
2025-10-23 10:26:33 +0300

    Update MilKilometreTablosu.java

---
+++ Task-5-TabloDesenleri/MilKilometreTablosu.java
@@ -0,0 +1,13 @@
+package doonusumtablosu;
+
+public class donusumtablosu {
+	public static void main(String[] args) {
+double mil=1.609 ;
+System.out.println("DÖNÜŞÜM"+"                              "+ "TABLOSU");
+System.out.println("===================================================");
+System.out.println("MİL   "+"1"+" / "+"5"+"   /"+"10"+"   /  "+"20"+"   / "+"50");
+System.out.println("(KM)" +mil+"/"+5*mil+"/"+10*mil+"/"+20*mil+"/"+50*mil);
+
+
+	}
+}
