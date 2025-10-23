7dd59f3d7b922ab2b01deec687f3f55336c4fc9c
ümitcan <cinarx04@gmail.com>
2025-10-23 10:10:55 +0300

    Update ToplamaSeri.java

---
+++ Task-2-Matematikselislemler/ToplamaSeri.java
@@ -0,0 +1,8 @@
+package toplamaserisi;
+
+public class Toplamaserisi {
+	public static void main(String[] args) {
+		int sonuç =1+2+3+4+5+6+7+8+9;
+		System.out.println("1den 9a kadar sayıların toplamı= "+ sonuç);
+	}
+}
