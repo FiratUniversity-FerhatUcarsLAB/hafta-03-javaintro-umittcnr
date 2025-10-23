103f455bd3d9a1272f5a026fec9998361a10e725
ümitcan <cinarx04@gmail.com>
2025-10-23 10:30:59 +0300

    Update VucutKitleIndeksi.java

---
+++ TASK-BONUS/VucutKitleIndeksi.java
@@ -0,0 +1,10 @@
+package bmi;
+
+public class bmi {
+	 public static void main(String[] args) {
+		 double boy =1.85;
+		 double kilo =80;
+				double bmi =kilo/(boy*boy);
+	 System.out.println("185 boy ve 80 kiloya sahip birinin BMisi= "+ bmi );
+	 }
+}
