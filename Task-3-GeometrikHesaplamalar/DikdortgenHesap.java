7ae01cd8eebf61210a6aa61961d1fe8ef4be720d
ümitcan <cinarx04@gmail.com>
2025-10-23 10:15:01 +0300

    Update DikdortgenHesap.java

---
+++ Task-3-GeometrikHesaplamalar/DikdortgenHesap.java
@@ -0,0 +1,12 @@
+package dikdortgen;
+
+public class dikdortgen {
+	public static void main(String[] args) {
+       double en=4.5 ;
+       double boy=7.9 ;
+       double alan=en*boy ;
+       double çevre= 2*en +2*boy;
+       System.out.println("dikdörtgenin alanı= " +alan);
+	   System.out.println("dikdörtgenin çevresi= " +çevre );
+	}
+}
